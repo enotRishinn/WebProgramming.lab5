8665cbb359b554625eaaae908fe33da99b63368e
Emtseva Victoria <enot.rishinn@yandex.ru>
2019-05-07 19:19:31 +0300

    25 commit

--- lab-node-js/express-app/routes/index.js
+++ lab-node-js/express-app/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Markdown Editor' });
 });
 
 module.exports = router;
