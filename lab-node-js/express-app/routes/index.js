a020f425ce34b0f9990a335511571cf0a3235b9a
Emtseva Victoria <enot.rishinn@yandex.ru>
2019-05-07 20:49:53 +0300

    26 commit

--- lab-node-js/express-app/routes/index.js
+++ lab-node-js/express-app/routes/index.js
@@ -1,10 +1,37 @@
 var express = require('express');
 var router = express.Router();
 var mongo = require('mongodb');
+var assert = require('assert');
+
+var url = 'mongodb://localhost:27017/test';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Markdown Editor' });
+  res.render('index', { title: 'Markdown Editor', fileName: null, fileText: null, myFilesArr: myFilesArr });
+});
+
+router.post('/add', function(req, res, next){
+  var fileName = req.body.addFile;
+  var item = {
+    filename: fileName
+  };
+  mongo.connect(url, function(err, client) {
+    assert.equal(null, error);
+    db.collection('files').insertOne(item, function(err, result) {
+      assert.equal(null, error);
+      db.close();
+
+    });
+  });
+
+});
+
+router.post('/delete', function(req, res, next){
+
+});
+
+router.post('/update', function(req, res, next){
+
 });
 
 module.exports = router;
